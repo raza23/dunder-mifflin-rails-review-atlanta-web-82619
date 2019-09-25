41d16cdb05b34fc455091a269e635b602b14c23e
Raza <rtshareef@gmail.com>
2019-09-25 15:36:58 -0400

    im gonna get right

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,29 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all 
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new 
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def edit 
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.create(first_name: first_name,last_name: last_name, alias: alias, office: office,img_url: img_url,dog_id: id)
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(first_name: first_name,last_name: last_name, alias: alias, office: office,img_url: img_url,dog_id: id)
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    def whole_name
+        self.first_name + ' ' + self.last_name
+    end
 end
